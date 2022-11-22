a1e65a3e61e816adcba37a11956a03d38ab811d6
wojtko <107132788+JakubgitaraNormalnie@users.noreply.github.com>
2022-11-22 11:47:55 +0000

    Add files via upload

---
+++ rules.pl
@@ -0,0 +1,659 @@
+city(Arouca).
+city(Espinho).
+city(Gondomar).
+city(Maia).
+city(Matosinhos).
+city(Oliveira de Azemeis).
+city(Paredes).
+city(Porto).
+city(Povoa de Varzim).
+city(Santa Maria da Feira).
+city(Santo Tirso).
+city(Sao Joao da Madeira).
+city(Trofa).
+city(Vale de Cambra).
+city(Valongo).
+city(Vila do Conde).
+city(Vila Nova de Gaia).
+
+idStore('Arouca', 1).
+idStore('Espinho', 2).
+idStore('Gondomar', 3).
+idStore('Maia', 4).
+idStore('Matosinhos', 5).
+idStore('Oliveira de Azemeis', 6).
+idStore('Paredes', 7).
+idStore('Porto', 8).
+idStore('Povoa de Varzim', 9).
+idStore('Santa Maria da Feira', 10).
+idStore('Santo Tirso', 11).
+idStore('Sao Joao da Madeira', 12).
+idStore('Trofa', 13).
+idStore('Vale de Cambra', 14).
+idStore('Valongo', 15).
+idStore('Vila do Conde', 16).
+idStore('Vila Nova de Gaia', 17).
+
+/dist(<IdCity>, <distance>, <SecondCityId>) /*
+dist(1, 0, 1).
+dist(1, 53, 2).
+dist(1, 57 3).
+dist(1, 67, 4).
+dist(1, 65, 5).
+dist(1, 30, 6).
+dist(1, 44, 7).
+dist(1, 57, 8).
+dist(1, 92, 9).
+dist(1, 37, 10).
+dist(1, 80, 11).
+dist(1, 29, 12).
+dist(1, 83, 13).
+dist(1, 22, 14).
+dist(1, 64, 15).
+dist(1, 85, 16).
+dist(1, 56, 17).
+
+dist(2, 52, 1).
+dist(2, 0, 2).
+dist(2, 28, 3).
+dist(2, 31, 4).
+dist(2, 27, 5).
+dist(2, 34, 6).
+dist(2, 48, 7).
+dist(2, 22, 8).
+dist(2, 55, 9).
+dist(2, 17, 10).
+dist(2, 51, 11).
+dist(2, 24, 12).
+dist(2, 53, 13).
+dist(2, 43, 14).
+dist(2, 37, 15).
+dist(2, 47, 16).
+dist(2, 18, 17).
+
+dist(3, 56, 1).
+dist(3, 28, 2).
+dist(3, 0, 3).
+dist(3, 19, 4).
+dist(3, 18, 5).
+dist(3, 46, 6).
+dist(3, 29, 7).
+dist(3, 10, 8).
+dist(3, 45, 9).
+dist(3, 35, 10).
+dist(3, 34, 11).
+dist(3, 40, 12).
+dist(3, 36, 13).
+dist(3, 47, 14).
+dist(3, 10, 15).
+dist(3, 38, 16).
+dist(3, 13, 17).
+
+dist(4, 68, 1).
+dist(4, 30, 2).
+dist(4, 20, 3).
+dist(4, 0, 4).
+dist(4, 12, 5).
+dist(4, 59, 6).
+dist(4, 34, 7).
+dist(4, 12, 8).
+dist(4, 32, 9).
+dist(4, 43, 10).
+dist(4, 24, 11).
+dist(4, 52, 12).
+dist(4, 14, 13).
+dist(4, 60, 14).
+dist(4, 16, 15).
+dist(4, 18, 16).
+dist(4, 14, 17).
+
+dist(5, 64, 1).
+dist(5, 25, 2).
+dist(5, 18, 3).
+dist(5, 11, 4).
+dist(5, 0, 5).
+dist(5, 55, 6).
+dist(5, 35, 7).
+dist(5, 9, 8).
+dist(5, 30, 9).
+dist(5, 37, 10).
+dist(5, 32, 11).
+dist(5, 45, 12).
+dist(5, 25, 13).
+dist(5, 56, 14).
+dist(5, 18, 15).
+dist(5, 23, 16).
+dist(5, 9, 17).
+
+dist(6, 9, 1).
+dist(6, 34, 2).
+dist(6, 47, 3).
+dist(6, 58, 4).
+dist(6, 55, 5).
+dist(6, 0, 6).
+dist(6, 60, 7).
+dist(6, 48, 8).
+dist(6, 83, 9).
+dist(6, 18, 10).
+dist(6, 70, 11).
+dist(6, 11, 12).
+dist(6, 73, 13).
+dist(6, 11, 14).
+dist(6, 55, 15).
+dist(6, 75, 16).
+dist(6, 47, 17).
+
+dist(7, 45, 1).
+dist(7, 48, 2).
+dist(7, 28, 3).
+dist(7, 32, 4).
+dist(7, 35, 5).
+dist(7, 60, 6).
+dist(7, 0, 7).
+dist(7, 33, 8).
+dist(7, 60, 9).
+dist(7, 55, 10).
+dist(7, 28, 11).
+dist(7, 53, 12).
+dist(7, 39, 13).
+dist(7, 61, 14).
+dist(7, 20, 15).
+dist(7, 52, 16).
+dist(7, 38, 17).
+
+dist(8, 58, 1).
+dist(8, 23, 2).
+dist(8, 8, 3).
+dist(8, 12, 4).
+dist(8, 9, 5).
+dist(8, 48, 6).
+dist(8, 32, 7).
+dist(8, 0, 8).
+dist(8, 37, 9).
+dist(8, 32, 10).
+dist(8, 28, 11).
+dist(8, 42, 12).
+dist(8, 30, 13).
+dist(8, 50, 14).
+dist(8, 15, 15).
+dist(8, 29, 16).
+dist(8, 7, 17).
+
+dist(9, 90, 1).
+dist(9, 51, 2).
+dist(9, 44, 3).
+dist(9, 30, 4).
+dist(9, 29, 5).
+dist(9, 81, 6).
+dist(9, 59, 7).
+dist(9, 35, 8).
+dist(9, 0, 9).
+dist(9, 64, 10).
+dist(9, 36, 11).
+dist(9, 71, 12).
+dist(9, 24, 13).
+dist(9, 82, 14).
+dist(9, 42, 15).
+dist(9, 4, 16).
+dist(9, 35, 17).
+
+dist(10, 37, 1).
+dist(10, 17, 2).
+dist(10, 34, 3).
+dist(10, 41, 4).
+dist(10, 38, 5).
+dist(10, 18, 6).
+dist(10, 53, 7).
+dist(10, 30, 8).
+dist(10, 65, 9).
+dist(10, 0, 10).
+dist(10, 57, 11).
+dist(10, 8, 12).
+dist(10, 58, 13).
+dist(10, 21, 14).
+dist(10, 43, 15).
+dist(10, 58, 16).
+dist(10, 29, 17).
+
+dist(11, 81, 1).
+dist(11, 51, 2).
+dist(11, 35, 3).
+dist(11, 22, 4).
+dist(11, 31, 5).
+dist(11, 71, 6).
+dist(11, 29, 7).
+dist(11, 29, 8).
+dist(11, 35, 9).
+dist(11, 57, 10).
+dist(11, 0, 11).
+dist(11, 65, 12).
+dist(11, 9, 13).
+dist(11, 73, 14).
+dist(11, 31, 15).
+dist(11, 34, 16).
+dist(11, 34, 17).
+
+dist(12, 29, 1).
+dist(12, 24, 2).
+dist(12, 40, 3).
+dist(12, 51, 4).
+dist(12, 48, 5).
+dist(12, 10, 6).
+dist(12, 53, 7).
+dist(12, 41, 8).
+dist(12, 76, 9).
+dist(12, 8, 10).
+dist(12, 63, 11).
+dist(12, 0, 12).
+dist(12, 66, 13).
+dist(12, 13, 14).
+dist(12, 48, 15).
+dist(12, 68, 16).
+dist(12, 40, 17).
+
+dist(13, 81, 1).
+dist(13, 44, 2).
+dist(13, 36, 3).
+dist(13, 14, 4).
+dist(13, 25, 5).
+dist(13, 71, 6).
+dist(13, 38, 7).
+dist(13, 25, 8).
+dist(13, 25, 9).
+dist(13, 59, 10).
+dist(13, 9, 11).
+dist(13, 65, 12).
+dist(13, 0, 13).
+dist(13, 73, 14).
+dist(13, 21, 15).
+dist(13, 19, 16).
+dist(13, 27, 17).
+
+dist(14, 22, 1).
+dist(14, 44, 2).
+dist(14, 48, 3).
+dist(14, 58, 4).
+dist(14, 56, 5).
+dist(14, 11, 6).
+dist(14, 60, 7).
+dist(14, 48, 8).
+dist(14, 83, 9).
+dist(14, 21, 10).
+dist(14, 71, 11).
+dist(14, 13, 12).
+dist(14, 73, 13).
+dist(14, 0, 14).
+dist(14, 55, 15).
+dist(14, 76, 16).
+dist(14, 47, 17).
+
+dist(15, 64, 1).
+dist(15, 38, 2).
+dist(15, 10, 3).
+dist(15, 15, 4).
+dist(15, 18, 5).
+dist(15, 55, 6).
+dist(15, 20, 7).
+dist(15, 16, 8).
+dist(15, 44, 9).
+dist(15, 45, 10).
+dist(15, 31, 11).
+dist(15, 48, 12).
+dist(15, 22, 13).
+dist(15, 56, 14).
+dist(15, 0, 15).
+dist(15, 36, 16).
+dist(15, 21, 17).
+
+dist(16, 85, 1).
+dist(16, 46, 2).
+dist(16, 39, 3).
+dist(16, 20, 4).
+dist(16, 23, 5).
+dist(16, 75, 6).
+dist(16, 53, 7).
+dist(16, 29, 8).
+dist(16, 4, 9).
+dist(16, 58, 10).
+dist(16, 35, 11).
+dist(16, 65, 12).
+dist(16, 19, 13).
+dist(16, 76, 14).
+dist(16, 36, 15).
+dist(16, 0, 16).
+dist(16, 30, 17).
+
+dist(17, 57, 1).
+dist(17, 18, 2).
+dist(17, 14, 3).
+dist(17, 16, 4).
+dist(17, 12, 5).
+dist(17, 47, 6).
+dist(17, 38, 7).
+dist(17, 8, 8).
+dist(17, 39, 9).
+dist(17, 30, 10).
+dist(17, 36, 11).
+dist(17, 37, 12).
+dist(17, 29, 13).
+dist(17, 48, 14).
+dist(17, 22, 15).
+dist(17, 31, 16).
+dist(17, 0, 17).
+
+
+
+/delivery(<id>, <date>, <deliveryMass>, <warehouseId>, <placementTime>, <receiveTime>)./
+delivery(4439, 20221205, 200, 1, 8, 10).
+delivery(4438, 20221205, 150, 9, 7, 9).
+delivery(4445, 20221205, 100, 3, 5, 7).
+delivery(4443, 20221205, 120, 8, 6, 8).
+delivery(4449, 20221205, 300, 11, 15, 20).
+/delivery(4398, 20221205, 310, 17, 16, 20).
+delivery(4432, 20221205, 270, 14, 14, 18).
+delivery(4437, 20221205, 180, 12, 9, 11).
+delivery(4451, 20221205, 220, 6, 9, 12).
+delivery(4452, 20221205, 390, 13, 21, 26).
+delivery(4444, 20221205, 380, 2, 20, 25).
+delivery(4455, 20221205, 280, 7, 14, 19).
+delivery(4399, 20221205, 260, 15, 13, 18).
+delivery(4454, 20221205, 350, 10, 18, 22).
+delivery(4446, 20221205, 260, 4, 14, 17).
+delivery(4456, 20221205, 330, 16, 17, 21)./ 
+
+truck(eTruck01).
+truck(eTruck02).
+truck(eTruck03).
+truck(eTruck04).
+truck(eTruck05).
+
+characteristicsTruck(eTruck01, 7500, 4300, 80, 100, 60). /truckname, tare, loadCap, totBattery, range, BatteryFrom20to80/*
+
+pathData(eTruck01,1,2,122,42,0).
+pathData(eTruck01,1,3,122,46,0).
+pathData(eTruck01,1,4,151,54,25).
+pathData(eTruck01,1,5,147,52,25).
+pathData(eTruck01,1,6,74,24,0).
+pathData(eTruck01,1,7,116,35,0).
+pathData(eTruck01,1,8,141,46,0).
+pathData(eTruck01,1,9,185,74,53).
+pathData(eTruck01,1,10,97,30,0).
+pathData(eTruck01,1,11,164,64,40).
+pathData(eTruck01,1,12,76,23,0).
+pathData(eTruck01,1,13,174,66,45).
+pathData(eTruck01,1,14,59,18,0).
+pathData(eTruck01,1,15,132,51,24).
+pathData(eTruck01,1,16,181,68,45).
+pathData(eTruck01,1,17,128,45,0).
+
+pathData(eTruck01,2,1,116,42,0).
+pathData(eTruck01,2,3,55,22,0).
+pathData(eTruck01,2,4,74,25,0).
+pathData(eTruck01,2,5,65,22,0).
+pathData(eTruck01,2,6,69,27,0).
+pathData(eTruck01,2,7,74,38,0).
+pathData(eTruck01,2,8,61,18,0).
+pathData(eTruck01,2,9,103,44,0).
+pathData(eTruck01,2,10,36,14,0).
+pathData(eTruck01,2,11,88,41,0).
+pathData(eTruck01,2,12,61,19,0).
+pathData(eTruck01,2,13,95,42,0).
+pathData(eTruck01,2,14,78,34,0).
+pathData(eTruck01,2,15,69,30,0).
+pathData(eTruck01,2,16,99,38,0).
+pathData(eTruck01,2,17,46,14,0).
+
+pathData(eTruck01,3,1,120,45,0).
+pathData(eTruck01,3,2,50,22,0).
+pathData(eTruck01,3,4,46,15,0).
+pathData(eTruck01,3,5,46,14,0).
+pathData(eTruck01,3,6,74,37,0).
+pathData(eTruck01,3,7,63,23,0).
+pathData(eTruck01,3,8,38,8,0).
+pathData(eTruck01,3,9,84,36,0).
+pathData(eTruck01,3,10,59,28,0).
+pathData(eTruck01,3,11,61,27,0).
+pathData(eTruck01,3,12,67,32,0).
+pathData(eTruck01,3,13,67,29,0).
+pathData(eTruck01,3,14,82,38,0).
+pathData(eTruck01,3,15,34,8,0).
+pathData(eTruck01,3,16,80,30,0).
+pathData(eTruck01,3,17,36,10,0).
+
+pathData(eTruck01,4,1,149,54,25).
+pathData(eTruck01,4,2,65,24,0).
+pathData(eTruck01,4,3,46,16,0).
+pathData(eTruck01,4,5,27,10,0).
+pathData(eTruck01,4,6,103,47,0).
+pathData(eTruck01,4,7,55,27,0).
+pathData(eTruck01,4,8,36,10,0).
+pathData(eTruck01,4,9,50,26,0).
+pathData(eTruck01,4,10,78,34,0).
+pathData(eTruck01,4,11,42,19,0).
+pathData(eTruck01,4,12,97,42,0).
+pathData(eTruck01,4,13,44,11,0).
+pathData(eTruck01,4,14,111,48,0).
+pathData(eTruck01,4,15,32,13,0).
+pathData(eTruck01,4,16,53,14,0).
+pathData(eTruck01,4,17,38,11,0).
+
+pathData(eTruck01,5,1,141,51,24).
+pathData(eTruck01,5,2,55,20,0).
+pathData(eTruck01,5,3,48,14,0).
+pathData(eTruck01,5,4,25,9,0).
+pathData(eTruck01,5,6,97,44,0).
+pathData(eTruck01,5,7,55,28,0).
+pathData(eTruck01,5,8,29,7,0).
+pathData(eTruck01,5,9,48,24,0).
+pathData(eTruck01,5,10,69,30,0).
+pathData(eTruck01,5,11,53,26,0).
+pathData(eTruck01,5,12,95,36,0).
+pathData(eTruck01,5,13,63,20,0).
+pathData(eTruck01,5,14,105,45,0).
+pathData(eTruck01,5,15,34,14,0).
+pathData(eTruck01,5,16,46,18,0).
+pathData(eTruck01,5,17,27,7,0).
+
+pathData(eTruck01,6,1,69,23,0).
+pathData(eTruck01,6,2,71,27,0).
+pathData(eTruck01,6,3,74,38,0).
+pathData(eTruck01,6,4,103,46,0).
+pathData(eTruck01,6,5,99,44,0).
+pathData(eTruck01,6,7,88,48,0).
+pathData(eTruck01,6,8,92,38,0).
+pathData(eTruck01,6,9,134,66,45).
+pathData(eTruck01,6,10,42,14,0).
+pathData(eTruck01,6,11,116,56,30).
+pathData(eTruck01,6,12,23,9,0).
+pathData(eTruck01,6,13,126,58,33).
+pathData(eTruck01,6,14,25,9,0).
+pathData(eTruck01,6,15,84,44,0).
+pathData(eTruck01,6,16,132,60,35).
+pathData(eTruck01,6,17,80,38,0).
+
+pathData(eTruck01,7,1,116,36,0).
+pathData(eTruck01,7,2,71,38,0).
+pathData(eTruck01,7,3,61,22,0).
+pathData(eTruck01,7,4,53,26,0).
+pathData(eTruck01,7,5,53,28,0).
+pathData(eTruck01,7,6,88,48,0).
+pathData(eTruck01,7,8,59,26,0).
+pathData(eTruck01,7,9,88,48,0).
+pathData(eTruck01,7,10,84,44,0).
+pathData(eTruck01,7,11,74,22,0).
+pathData(eTruck01,7,12,82,42,0).
+pathData(eTruck01,7,13,76,31,0).
+pathData(eTruck01,7,14,97,49,21).
+pathData(eTruck01,7,15,29,16,0).
+pathData(eTruck01,7,16,84,42,0).
+pathData(eTruck01,7,17,69,30,0).
+
+pathData(eTruck01,8,1,134,46,0).
+pathData(eTruck01,8,2,59,18,0).
+pathData(eTruck01,8,3,32,6,0).
+pathData(eTruck01,8,4,34,10,0).
+pathData(eTruck01,8,5,32,7,0).
+pathData(eTruck01,8,6,88,38,0).
+pathData(eTruck01,8,7,57,26,0).
+pathData(eTruck01,8,9,69,30,0).
+pathData(eTruck01,8,10,65,26,0).
+pathData(eTruck01,8,11,53,22,0).
+pathData(eTruck01,8,12,82,34,0).
+pathData(eTruck01,8,13,61,24,0).
+pathData(eTruck01,8,14,97,40,0).
+pathData(eTruck01,8,15,36,12,0).
+pathData(eTruck01,8,16,65,23,0).
+pathData(eTruck01,8,17,32,6,0).
+
+pathData(eTruck01,9,1,181,72,50).
+pathData(eTruck01,9,2,95,41,0).
+pathData(eTruck01,9,3,86,35,0).
+pathData(eTruck01,9,4,55,24,0).
+pathData(eTruck01,9,5,48,23,0).
+pathData(eTruck01,9,6,134,65,42).
+pathData(eTruck01,9,7,95,47,0).
+pathData(eTruck01,9,8,69,28,0).
+pathData(eTruck01,9,10,109,51,24).
+pathData(eTruck01,9,11,61,29,0).
+pathData(eTruck01,9,12,132,57,31).
+pathData(eTruck01,9,13,67,19,0).
+pathData(eTruck01,9,14,143,66,45).
+pathData(eTruck01,9,15,71,34,0).
+pathData(eTruck01,9,16,15,3,0).
+pathData(eTruck01,9,17,67,28,0).
+
+pathData(eTruck01,10,1,97,30,0).
+pathData(eTruck01,10,2,34,14,0).
+pathData(eTruck01,10,3,59,27,0).
+pathData(eTruck01,10,4,78,33,0).
+pathData(eTruck01,10,5,71,30,0).
+pathData(eTruck01,10,6,40,14,0).
+pathData(eTruck01,10,7,82,42,0).
+pathData(eTruck01,10,8,65,24,0).
+pathData(eTruck01,10,9,109,52,25).
+pathData(eTruck01,10,11,92,46,0).
+pathData(eTruck01,10,12,32,6,0).
+pathData(eTruck01,10,13,99,46,0).
+pathData(eTruck01,10,14,63,17,0).
+pathData(eTruck01,10,15,74,34,0).
+pathData(eTruck01,10,16,105,46,0).
+pathData(eTruck01,10,17,53,23,0).
+
+pathData(eTruck01,11,1,164,65,42).
+pathData(eTruck01,11,2,88,41,0).
+pathData(eTruck01,11,3,65,28,0).
+pathData(eTruck01,11,4,42,18,0).
+pathData(eTruck01,11,5,55,25,0).
+pathData(eTruck01,11,6,118,57,31).
+pathData(eTruck01,11,7,74,23,0).
+pathData(eTruck01,11,8,59,23,0).
+pathData(eTruck01,11,9,63,28,0).
+pathData(eTruck01,11,10,97,46,0).
+pathData(eTruck01,11,12,111,52,25).
+pathData(eTruck01,11,13,25,7,0).
+pathData(eTruck01,11,14,126,58,33).
+pathData(eTruck01,11,15,53,25,0).
+pathData(eTruck01,11,16,59,27,0).
+pathData(eTruck01,11,17,67,27,0).
+
+pathData(eTruck01,12,1,76,23,0).
+pathData(eTruck01,12,2,61,19,0).
+pathData(eTruck01,12,3,67,32,0).
+pathData(eTruck01,12,4,97,41,0).
+pathData(eTruck01,12,5,92,38,0).
+pathData(eTruck01,12,6,19,8,0).
+pathData(eTruck01,12,7,82,42,0).
+pathData(eTruck01,12,8,86,33,0).
+pathData(eTruck01,12,9,128,61,37).
+pathData(eTruck01,12,10,32,6,0).
+pathData(eTruck01,12,11,109,50,23).
+pathData(eTruck01,12,13,120,53,26).
+pathData(eTruck01,12,14,40,10,0).
+pathData(eTruck01,12,15,78,38,0).
+pathData(eTruck01,12,16,126,54,28).
+pathData(eTruck01,12,17,74,32,0).
+
+pathData(eTruck01,13,1,174,65,42).
+pathData(eTruck01,13,2,107,35,0).
+pathData(eTruck01,13,3,74,29,0).
+pathData(eTruck01,13,4,46,11,0).
+pathData(eTruck01,13,5,67,20,0).
+pathData(eTruck01,13,6,128,57,31).
+pathData(eTruck01,13,7,80,30,0).
+pathData(eTruck01,13,8,76,20,0).
+pathData(eTruck01,13,9,67,20,0).
+pathData(eTruck01,13,10,105,47,0).
+pathData(eTruck01,13,11,27,7,0).
+pathData(eTruck01,13,12,122,52,25).
+pathData(eTruck01,13,14,137,58,33).
+pathData(eTruck01,13,15,67,17,0).
+pathData(eTruck01,13,16,59,15,0).
+pathData(eTruck01,13,17,78,22,0).
+
+pathData(eTruck01,14,1,59,18,0).
+pathData(eTruck01,14,2,80,35,0).
+pathData(eTruck01,14,3,80,38,0).
+pathData(eTruck01,14,4,109,46,0).
+pathData(eTruck01,14,5,105,45,0).
+pathData(eTruck01,14,6,27,9,0).
+pathData(eTruck01,14,7,97,48,0).
+pathData(eTruck01,14,8,99,38,0).
+pathData(eTruck01,14,9,143,66,45).
+pathData(eTruck01,14,10,61,17,0).
+pathData(eTruck01,14,11,122,57,31).
+pathData(eTruck01,14,12,42,10,0).
+pathData(eTruck01,14,13,132,58,35).
+pathData(eTruck01,14,15,90,44,0).
+pathData(eTruck01,14,16,139,61,37).
+pathData(eTruck01,14,17,86,38,0).
+
+pathData(eTruck01,15,1,132,51,24).
+pathData(eTruck01,15,2,74,30,0).
+pathData(eTruck01,15,3,34,8,0).
+pathData(eTruck01,15,4,36,12,0).
+pathData(eTruck01,15,5,36,14,0).
+pathData(eTruck01,15,6,86,44,0).
+pathData(eTruck01,15,7,34,16,0).
+pathData(eTruck01,15,8,42,13,0).
+pathData(eTruck01,15,9,71,35,0).
+pathData(eTruck01,15,10,82,36,0).
+pathData(eTruck01,15,11,53,25,0).
+pathData(eTruck01,15,12,80,38,0).
+pathData(eTruck01,15,13,69,18,0).
+pathData(eTruck01,15,14,95,45,0).
+pathData(eTruck01,15,16,69,29,0).
+pathData(eTruck01,15,17,53,17,0).
+
+pathData(eTruck01,16,1,179,68,45).
+pathData(eTruck01,16,2,92,37,0).
+pathData(eTruck01,16,3,84,31,0).
+pathData(eTruck01,16,4,57,16,0).
+pathData(eTruck01,16,5,46,18,0).
+pathData(eTruck01,16,6,132,60,35).
+pathData(eTruck01,16,7,92,42,0).
+pathData(eTruck01,16,8,67,23,0).
+pathData(eTruck01,16,9,15,3,0).
+pathData(eTruck01,16,10,105,46,0).
+pathData(eTruck01,16,11,57,28,0).
+pathData(eTruck01,16,12,130,52,25).
+pathData(eTruck01,16,13,61,15,0).
+pathData(eTruck01,16,14,141,61,37).
+pathData(eTruck01,16,15,69,29,0).
+pathData(eTruck01,16,17,65,24,0).
+
+pathData(eTruck01,17,1,128,46,0).
+pathData(eTruck01,17,2,42,14,0).
+pathData(eTruck01,17,3,40,11,0).
+pathData(eTruck01,17,4,42,13,0).
+pathData(eTruck01,17,5,34,10,0).
+pathData(eTruck01,17,6,82,38,0).
+pathData(eTruck01,17,7,74,30,0).
+pathData(eTruck01,17,8,29,6,0).
+pathData(eTruck01,17,9,69,31,0).
+pathData(eTruck01,17,10,55,24,0).
+pathData(eTruck01,17,11,69,29,0).
+pathData(eTruck01,17,12,80,30,0).
+pathData(eTruck01,17,13,82,23,0).
+pathData(eTruck01,17,14,90,38,0).
+pathData(eTruck01,17,15,53,18,0).
+pathData(eTruck01,17,16,67,25,0).
