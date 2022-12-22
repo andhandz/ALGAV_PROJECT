fa456d16fc8ecfd0a6c8bbc9d1d6c7792bb4c2f9
andhandz <andhandz0125@gmail.com>
2022-12-22 12:27:23 +0000

    add basic GA in english

--- rules.pl
+++ rules.pl
@@ -333,7 +333,7 @@ delivery(4445, 20221205, 100, 3, 5, 7).
 delivery(4443, 20221205, 120, 8, 6, 8).
 delivery(4449, 20221205, 300, 11, 15, 20).
 delivery(4398, 20221205, 310, 17, 16, 20).
-delivery(4432, 20221205, 270, 14, 14, 18).
+/*delivery(4432, 20221205, 270, 14, 14, 18).
 delivery(4437, 20221205, 180, 12, 9, 11).
 delivery(4451, 20221205, 220, 6, 9, 12).
 delivery(4452, 20221205, 390, 13, 21, 26).
@@ -342,7 +342,7 @@ delivery(4455, 20221205, 280, 7, 14, 19).
 delivery(4399, 20221205, 260, 15, 13, 18).
 delivery(4454, 20221205, 350, 10, 18, 22).
 delivery(4446, 20221205, 260, 4, 14, 17).
-delivery(4456, 20221205, 330, 16, 17, 21).
+delivery(4456, 20221205, 330, 16, 17, 21).*/
 
 truck(eTruck01).
 truck(eTruck02).
@@ -805,4 +805,249 @@ find_the_lowest(C1,[CC|LC],V,C):-
 calculate_combine_cost(C1,C2,V):- dist(C1,D,C2), delivery(_,_,W,C2,_,_), V is D/W,!. 	
 
 
+ 
+ /* SPRINT C */
+
+
+% task(Id,Length,Deadline,Penalty).
+task(t1,2,5,1).
+task(t2,4,7,6).
+task(t3,1,11,2).
+task(t4,3,9,3).
+task(t5,3,8,2).
+
+% tasks(NTasks).
+tasks(5).
+
+/* parameters init */
+
+init:-write('Number of New Generations: '),read(NG), 			(retract(generation(_));true), assertz(generation(NG)),
+	write('Population Size: '),read(DP),
+	(retract(population(_));true), assertz(population(DP)),
+	write('Crossover Probability (%):'), read(P1),
+	PC is P1/100, 
+	(retract(prob_cross(_));true), 	assertz(prob_cross(PC)),
+	write('Mutation Probability (%):'), read(P2),
+	PM is P2/100, 
+	(retract(prob_mut(_));true), assertz(prob_mut(PM)).
+
+
+generate:-
+	init,
+	generate_population(Pop),
+	write('Pop='),write(Pop),nl,
+	evaluate_population(Pop,PopEv),
+	write('PopEv='),write(PopEv),nl,
+	order_population(PopEv,PopOrd),
+	generation(NG),
+	generate_generation(0,NG,PopOrd).
+
+generate_population(Pop):-
+	population(TamPop),
+	tasks(NumT),
+	findall(Task,task(Task,_,_,_),LTasks),
+	generate_population(TamPop,LTasks,NumT,Pop).
+
+generate_population(0,_,_,[]):-!.
+
+generate_population(TamPop,LTasks,NumT,[Ind|Rest]):-
+	TamPop1 is TamPop-1,
+	generate_population(TamPop1,LTasks,NumT,Rest),
+	generate_individual(LTasks,NumT,Ind),
+	not(member(Ind,Rest)).
+generate_population(TamPop,LTasks,NumT,L):-
+	generate_population(TamPop,LTasks,NumT,L).
+
+generate_individual([G],1,[G]):-!.
+
+generate_individual(LTasks,NumT,[G|Rest]):-
+	NumTemp is NumT + 1, % To use with random
+	random(1,NumTemp,N),
+	remove(N,LTasks,G,NewL),
+	NumT1 is NumT-1,
+	generate_individual(NewL,NumT1,Rest).
+
+remove(1,[G|Rest],G,Rest).
+remove(N,[G1|Rest],G,[G1|Rest1]):-
+	N1 is N-1,
+	remove(N1,Rest,G,Rest1).
+
+evaluate_population([],[]).
+evaluate_population([Ind|Rest],[Ind*V|Rest1]):-
+	evaluate(Ind,V),
+	evaluate_population(Rest,Rest1).
+
+evaluate(Seq,V):-
+	evaluate(Seq,0,V).
+
+evaluate([],_,0).
+evaluate([T|Rest],Inst,V):-
+	task(T,Dur,Deadline,Pen),
+	InstSum is Inst+Dur,
+	evaluate(Rest,InstSum,VRest),
+	(
+		(InstSum =< Deadline,!, VT is 0)
+  ;
+		(VT is (InstSum-Deadline)*Pen)
+	),
+	V is VT+VRest.
+
+order_population(PopEv,PopEvOrd):-
+	bsort(PopEv,PopEvOrd).
+
+bsort([X],[X]):-!.
+bsort([X|Xs],Ys):-
+	bsort(Xs,Zs),
+	bexchange([X|Zs],Ys).
+
+
+bexchange([X],[X]):-!.
+
+bexchange([X*VX,Y*VY|L1],[Y*VY|L2]):-
+	VX>VY,!,
+	bexchange([X*VX|L1],L2).
+
+bexchange([X|L1],[X|L2]):-bexchange(L1,L2).
+
+
+generate_generation(G,G,Pop):-!,
+	write('Generate'), write(G), write(':'), nl, write(Pop), nl.
+
+generate_generation(N,G,Pop):-
+	write('Generate '), write(N), write(':'), nl, write(Pop), nl,
+	cross(Pop,NPop1),
+	mutation(NPop1,NPop),
+	evaluate_population(NPop,NPopEv),
+	order_population(NPopEv,NPopOrd),
+	N1 is N+1,
+	generate_generation(N1,G,NPopOrd).
+
+generate_points_cross(P1,P2):-
+	generate_points_cross1(P1,P2).
+
+generate_points_cross1(P1,P2):-
+	tasks(N),
+	NTemp is N+1,
+	random(1,NTemp,P11),
+	random(1,NTemp,P21),
+	P11\==P21,!,
+	((P11<P21,!,P1=P11,P2=P21);(P1=P21,P2=P11)).
+generate_points_cross1(P1,P2):-
+	generate_points_cross1(P1,P2).
+
+
+cross([],[]).
+cross([Ind*_],[Ind]).
+cross([Ind1*_,Ind2*_|Rest],[NInd1,NInd2|Rest1]):-
+	generate_points_cross(P1,P2),
+	prob_cross(Pcrs),random(0.0,1.0,Pc),
+	((Pc =< Pcrs,!,
+        crs(Ind1,Ind2,P1,P2,NInd1),
+	  crs(Ind2,Ind1,P1,P2,NInd2))
+	;
+	(NInd1=Ind1,NInd2=Ind2)),
+	cross(Rest,Rest1).
+
+preencheh([],[]).
+
+preencheh([_|R1],[h|R2]):-
+	preencheh(R1,R2).
+
+
+sublist(L1,I1,I2,L):-
+	I1 < I2,!,
+	sublist1(L1,I1,I2,L).
+
+sublist(L1,I1,I2,L):-
+	sublist1(L1,I2,I1,L).
+
+sublist1([X|R1],1,1,[X|H]):-!,
+	preencheh(R1,H).
+
+sublist1([X|R1],1,N2,[X|R2]):-!,
+	N3 is N2 - 1,
+	sublist1(R1,1,N3,R2).
+
+sublist1([_|R1],N1,N2,[h|R2]):-
+	N3 is N1 - 1,
+	N4 is N2 - 1,
+	sublist1(R1,N3,N4,R2).
+
+rotate_right(L,K,L1):-
+	tasks(N),
+	T is N - K,
+	rr(T,L,L1).
+
+rr(0,L,L):-!.
+
+rr(N,[X|R],R2):-
+	N1 is N - 1,
+	append(R,[X],R1),
+	rr(N1,R1,R2).
+
+
+eliminate([],_,[]):-!.
+
+eliminate([X|R1],L,[X|R2]):-
+	not(member(X,L)),!,
+	eliminate(R1,L,R2).
+
+eliminate([_|R1],L,R2):-
+	eliminate(R1,L,R2).
+
+insert([],L,_,L):-!.
+insert([X|R],L,N,L2):-
+	tasks(T),
+	((N>T,!,N1 is N mod T);N1 = N),
+	insert1(X,N1,L,L1),
+	N2 is N + 1,
+	insert(R,L1,N2,L2).
+
+
+insert1(X,1,L,[X|L]):-!.
+insert1(X,N,[Y|L],[Y|L1]):-
+	N1 is N-1,
+	insert1(X,N1,L,L1).
+
+crs(Ind1,Ind2,P1,P2,NInd11):-
+	sublist(Ind1,P1,P2,Sub1),
+	tasks(NumT),
+	R is NumT-P2,
+	rotate_right(Ind2,R,Ind21),
+	eliminate(Ind21,Sub1,Sub2),
+	P3 is P2 + 1,
+	insert(Sub2,Sub1,P3,NInd1),
+	eliminate1(NInd1,NInd11).
+
+
+eliminate1([],[]).
+
+eliminate1([h|R1],R2):-!,
+	eliminate1(R1,R2).
+
+eliminate1([X|R1],[X|R2]):-
+	eliminate1(R1,R2).
+
+mutation([],[]).
+mutation([Ind|Rest],[NInd|Rest1]):-
+	prob_mut(Pmut),
+	random(0.0,1.0,Pm),
+	((Pm < Pmut,!,mutation1(Ind,NInd));NInd = Ind),
+	mutation(Rest,Rest1).
+
+mutation1(Ind,NInd):-
+	generate_points_cross(P1,P2),
+	mutation22(Ind,P1,P2,NInd).
+
+mutation22([G1|Ind],1,P2,[G2|NInd]):-
+	!, P21 is P2-1,
+	mutation23(G1,P21,Ind,G2,NInd).
+mutation22([G|Ind],P1,P2,[G|NInd]):-
+	P11 is P1-1, P21 is P2-1,
+	mutation22(Ind,P11,P21,NInd).
+
+mutation23(G1,1,[G2|Ind],G2,[G1|Ind]):-!.
+mutation23(G1,P,[G|Ind],G2,[G|NInd]):-
+	P1 is P-1,
+	mutation23(G1,P1,Ind,G2,NInd).
  
